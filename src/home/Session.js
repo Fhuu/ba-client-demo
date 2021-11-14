d81fd0d0e58d1e0dea36fd8982ec8e50a723366d
Diro Baloska <dirobaloska@outlook.com>
2021-11-14 20:38:41 +0100

    start when user is authorized

--- src/home/Session.js
+++ src/home/Session.js
@@ -2,17 +2,22 @@ import React from 'react';
 
 import { checkAuth } from '../helper/Auth';
 import Button from '../builder/PositiveButtonBuilder';
+import StopButton from '../builder/NegativeButtonBuilder';
 
 
 export default class Session extends React.Component {
 
 	constructor(props) {
 		super(props);
+
 		this.state = {
-			type : 'work',
-			timer : 60,
+			work : true,
+			timer : 1,
 			buttontype : 'start',
+			workcircle : 25,
+			restcircle : 5
 		}
+
 		this.countdown = null;
 	}
 
@@ -25,8 +30,7 @@ export default class Session extends React.Component {
 					timer : this.state.timer - 1
 				}, () => {
 					if(this.state.timer === 0) {
-						this.completeTask();
-						clearInterval(this.countdown);
+						this.changeSessionTime();
 					}
 				});
 			}, 1000);
@@ -34,17 +38,19 @@ export default class Session extends React.Component {
 	}
 
 	startSession = async () => {
-		await checkAuth();
-		this.startCountdown();
-		fetch('/v1/task', {
-			method: 'POST',
-			headers : {
-				'content-type' : 'application/json'
-			},
-			body : JSON.stringify({
-				title : document.getElementById('task-title').value
-			})
-		});
+		let user = await checkAuth();
+		if (user) {
+			this.startCountdown();
+			fetch('/v1/task', {
+				method: 'POST',
+				headers : {
+					'content-type' : 'application/json'
+				},
+				body : JSON.stringify({
+					title : document.getElementById('task-title').value
+				})
+			});
+		}
 
 	}
 
@@ -55,9 +61,32 @@ export default class Session extends React.Component {
 		});
 	}
 
+	stopSession = () => {
+		clearInterval(this.countdown);
+		this.setState({
+			buttontype : 'start',
+			work : true,
+			timer : this.state.workcircle * 60
+		})
+	}
+
 	completeTask = () => {
 		fetch('/v1/task/complete', {
 			method: 'GET'
+		});
+	}
+
+	changeSessionTime = () => {
+		this.setState({
+			work : !this.state.work
+		}, () => {
+			
+			if(this.state.work) {
+				this.setState({timer : this.state.workcircle * 60});
+				return;
+			}
+
+			this.setState({timer: this.state.restcircle *  60});
 		});
 	}
 
@@ -75,12 +104,18 @@ export default class Session extends React.Component {
 
 	button = () => {
 		if(this.state.buttontype === 'start') return <Button message="Start timer" callback={this.startSession} />;
-		return <Button message="Pause Timer" callback={this.pauseSession}></Button>
+		return (
+			<>
+				<Button message="Pause Timer" callback={this.pauseSession}></Button>
+				<StopButton message="Stop Timer" callback={this.stopSession}></StopButton>
+			</>
+		);
 	}
 	
 	render() {
 		return(
 			<div className="flex flex-col">
+				{this.state.buttontype !== 'start' ? this.state.work ? <span>Time to concenctrate</span> : <span>Take it easy for now</span> : <span className="invisible">invisible</span>}
 				<span>{this.renderMinutes()} : {this.renderSeconds()}</span>
 				<input type="text" placeholder="Title" id="task-title" />
 				{this.button()}
