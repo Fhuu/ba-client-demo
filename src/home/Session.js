eb4c91f38c0a60dac84bd07ca7aad338c6adaf0f
Diro Baloska <dirobaloska@outlook.com>
2021-11-14 12:21:59 +0100

    start and pause session

--- src/home/Session.js
+++ src/home/Session.js
@@ -1,8 +1,38 @@
 import React from 'react';
-import Button from '../builder/PositivevButtonBuilder';
+
+import { checkAuth } from '../helper/Auth';
+import Button from '../builder/PositiveButtonBuilder';
+
 
 export default class Session extends React.Component {
-	startSession = () => {
+
+	constructor(props) {
+		super(props);
+		this.state = {
+			type : 'work',
+			timer : 60,
+			buttontype : 'start',
+		}
+		this.countdown = null;
+	}
+
+	startCountdown = () => {
+		this.setState({
+			buttontype : 'interrupt'
+		}, () => {
+			this.countdown = setInterval(() => {
+				this.setState({
+					timer : this.state.timer - 1
+				}, () => {
+					if(this.state.timer === 0) clearInterval(this.countdown);
+				});
+			}, 1000);
+		});
+	}
+
+	startSession = async () => {
+		await checkAuth();
+		this.startCountdown();
 		fetch('/v1/task', {
 			method: 'POST',
 			headers : {
@@ -12,12 +42,39 @@ export default class Session extends React.Component {
 				title : document.getElementById('task-title').value
 			})
 		});
+
 	}
+
+	pauseSession = () => {
+		clearInterval(this.countdown);
+		this.setState({
+			buttontype : 'start'
+		});
+	}
+
+	renderSeconds = () => {
+		let seconds = this.state.timer % 60;
+		if(seconds <= 9) return `0${seconds}`;
+		return seconds;
+	}
+	
+	renderMinutes = () => {
+		let minutes = parseInt(this.state.timer / 60);
+		if(minutes <= 9) return `0${minutes}`;
+		return minutes;
+	}
+
+	button = () => {
+		if(this.state.buttontype === 'start') return <Button message="Start timer" callback={this.startSession} />;
+		return <Button message="Pause Timer" callback={this.pauseSession}></Button>
+	}
+	
 	render() {
 		return(
 			<div className="flex flex-col">
+				<span>{this.renderMinutes()} : {this.renderSeconds()}</span>
 				<input type="text" placeholder="Title" id="task-title" />
-				<Button message="Start timer" callback={this.startSession} />
+				{this.button()}
 			</div>
 		);
 	}
