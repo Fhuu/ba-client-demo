cb6f7ef9062458cefc0482fa31c2ff27a5745136
Diro Baloska <dirobaloska@outlook.com>
2021-11-14 12:24:00 +0100

    authenticate current user

--- src/helper/Auth.js
+++ src/helper/Auth.js
@@ -1,9 +1,14 @@
 async function checkAuth () {
-	let authorization = await fetch('/v1/user/authr', {method: 'GET'});
-	console.log(authorization.status);
-	let data = await authorization.json();
-	console.log(data.data);
-	if(authorization.status === 401) window.location.replace('/login');
+	let authorization = await fetch('/v1/user', {method: 'GET'});
+	switch(authorization.status) {
+		case 401 : 
+			if(window.location.pathname !== '/login' || window.location.pathname !== '/signup') window.location.replace('/login');
+			break;
+
+		case 200 : 
+			return (await authorization.json()).user;
+		
+	}
 }
 
 module.exports = {
