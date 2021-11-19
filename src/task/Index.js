9b05751c8faa13bcf7763ac5bdadea636a84611c
Diro Baloska <dirobaloska@outlook.com>
2021-11-19 16:56:26 +0100

    add task page

--- src/task/Index.js
+++ src/task/Index.js
@@ -1,16 +1,59 @@
 import React from 'react';
 import { checkAuth } from '../helper/Auth';
+import Task from '../builder/Task';
 
-export default class Task extends React.Component {
+export default class TaskPage extends React.Component {
 
-	componentDidMount = () => {
-		checkAuth();
+	constructor(props) {
+		super(props);
+		this.state = {
+			user : null,
+			tasks : []
+		}
+	}
+
+	componentDidMount = async () => {
+		if(this.state.user == null) {
+			this.setState({
+				user : await checkAuth()
+			})
+		}
+
+ 		this.getMyTasks();
+	}
+
+	getMyTasks = async () => {
+		if(this.state.user) {
+			let tasksRequest = await fetch('/v1/task/me', {
+				method : 'GET'
+			});
+			
+			let tasks = await tasksRequest.json();
+
+			this.setState({
+				tasks : tasks
+			});
+		}
+	}
+
+	renderTasks = () => {
+		return(
+			<div className="flex justify-center items-center">
+				<ul className="flex flex-col justify-center items-start">
+					{
+						this.state.tasks.map(task => {
+							return (<Task key={task._id} task={task}></Task>)
+						})
+					}
+				</ul>
+			</div>
+		);
 	}
 
 	render() {
 		return(
 			<div>
-				<p>This is task index</p>
+				{this.renderTasks()}
 			</div>
 		);
 	}
