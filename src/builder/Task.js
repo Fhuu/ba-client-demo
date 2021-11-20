a5bf4a1538f6413706a9b30ff7145534ff5bfbf7
Diro Baloska <dirobaloska@outlook.com>
2021-11-20 17:04:14 +0100

    add events to task component

--- src/builder/Task.js
+++ src/builder/Task.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import NegativeButton from './NegativeButtonBuilder';
+import './Task.css';
 
 export default class Task extends React.Component {
 
@@ -8,15 +10,28 @@ export default class Task extends React.Component {
 		if(completeRequest.status === 201 || completeRequest.status === 200) this.props.refreshTasks();
 	}
 
+	activateDelete = () => {
+		document.getElementById(`delete-button-${this.props.task._id}`).classList.add('active');
+	}
+
+	deactiveDelete = () => {
+		document.getElementById(`delete-button-${this.props.task._id}`).classList.remove('active');
+	}
+
 	render() {
 		return(
-			<li key={this.props.task._id}>
-				{
-					this.props.task.completed ? 
-					<input type="checkbox" className="mr-4" checked onChange={this.changeCompletion}/> :
-					<input type="checkbox" className="mr-4" onChange={this.changeCompletion}/>
-				}
-				<label>{this.props.task.title}</label>
+			<li key={this.props.task._id} className="flex items-center w-full">
+				<section id="task-container" onMouseEnter={this.activateDelete} onMouseLeave={this.deactiveDelete}>
+					{
+						this.props.task.completed ? 
+						<input type="checkbox" className="mr-4" checked onChange={this.changeCompletion}/> :
+						<input type="checkbox" className="mr-4" onChange={this.changeCompletion}/>
+					}
+					<label id="task-label">{this.props.task.title}</label>
+					<div className="delete-buttons" id={`delete-button-${this.props.task._id}`}>
+						<NegativeButton message="delete"></NegativeButton>
+					</div>
+				</section>
 			</li>
 		);
 	}
