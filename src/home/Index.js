081669034e1daca7c1dfec0d499bc852ad2a7487
Diro Baloska <dirobaloska@outlook.com>
2021-11-20 17:05:47 +0100

    import css

--- src/home/Index.js
+++ src/home/Index.js
@@ -1,6 +1,7 @@
 import react from 'react';
 import Session from './session/Session';
 import Tasks from './task/Tasks';
+import './index.css';
 
 export default class Home extends react.Component {
 
